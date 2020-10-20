d778410630b9fa62ac85737b56b6d9ce0d19dfa7
gaojunbin <913290959@qq.com>
2020-10-20 22:20:11 +0800

    modify: 实时检测可用版本

--- realtime_detect.py
+++ realtime_detect.py
@@ -31,19 +31,26 @@ def detect_cv2(m,imgfile,use_cuda):
 
 
 def real_time():
-    # cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture(0)
     cfgfile = "cfg/yolov2.cfg"
     weightfile = "weights/yolo.weights"
     m = Darknet(cfgfile)
     m.load_weights(weightfile)
-    # ret = True
-    cap = cv2.VideoCapture("test.mov")
-    while cap.isOpened():
+    ret = True
+    cv2.namedWindow("test",0)
+    cv2.resizeWindow("test",800,800)
+    # cap = cv2.VideoCapture("test.mov")
+    # while cap.isOpened():
+    while ret:
         ret,frame = cap.read()
         frame = detect_cv2(m, frame,True)
-        cv2.imshow('实时目标检测',frame)
-        timenow=time.time()
-        cv2.imwrite('result/test.png',frame,[int(cv2.IMWRITE_PNG_COMPRESSION),9])
+        cv2.imshow('test',frame)
+        if cv2.waitKey(1) & 0xff ==ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+        # timenow=time.time()
+        # cv2.imwrite('result/test.png',frame,[int(cv2.IMWRITE_PNG_COMPRESSION),9])
 
 
 if __name__ == '__main__':
